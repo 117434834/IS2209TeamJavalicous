a67d78674443fc173ebb16999caa38d43c093423
117459314 <117459314@BISBIS-AUT6S183.bis.ad.ucc.ie>
2019-02-13 11:30:24 +0000

    Chloe update

--- src/is2209teamjavalicous/IS2209TeamJavalicous.java
+++ src/is2209teamjavalicous/IS2209TeamJavalicous.java
@@ -22,12 +22,12 @@ public class IS2209TeamJavalicous extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'Javaliciossss'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Javaliciossss");
             }
         });
         
@@ -36,7 +36,7 @@ public class IS2209TeamJavalicous extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Javaliciossss");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
